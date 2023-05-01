0152826f5c8abcc102fa33cecd369e33c9ded055
Piotr Yanushkevich <1999301py@gmail.com>
2023-05-01 15:56:13 +0300

    add hw

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,9 +1,18 @@
-const initState = {
-    isLoading: false,
+export type InitStateType = {
+    isLoading:  boolean
+}
+ const initState = {
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
+        case "CHANGE_LOADING":
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
+
         // пишет студент  // need to fix
 
         default:
